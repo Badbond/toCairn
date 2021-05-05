406bb6e7defe1015d6cddd6481314a75889bf35f
Pieter Dirk Soels <p.d.soels@hotmail.com>
2021-05-05 17:22:53 +0200

    Add mock input data and normalized clustering

--- src/main/java/me/soels/thesis/ApplicationInput.java
+++ src/main/java/me/soels/thesis/ApplicationInput.java
@@ -1,7 +1,17 @@
 package me.soels.thesis;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * The input for the multi-objective evolutionary algorithm.
@@ -9,23 +19,51 @@ import java.util.List;
  * This input is to be constructed from initial analysis of the application that we wish to identify microservice
  * boundaries for.
  */
+// TODO:
+//  This class was created as mock input data. We should determine whether the current structure is acceptable.
+//  Questions left open: How do we model data objects? How do we incorporate additional information such as data size
+//                       and dependency frequency? Should we model without Strings?
 public class ApplicationInput {
-    private final List<String> classes;
+    private final List<String> classes = new ArrayList<>();
+    private final List<Pair<String, String>> edges = new ArrayList<>();
 
     public ApplicationInput() {
-        this.classes = new ArrayList<>(100);
-        for (int i = 0; i < 100; i++) {
-            classes.add("Class" + i);
+        // Temporary hard-coded graph from graph.csv
+        constructGraph();
+    }
+
+    private void constructGraph() {
+        var graphLines = getGraphString().split("\n");
+        var classMapping = new LinkedHashMap<String, String>();
+        for (var line : graphLines) {
+            var split = Arrays.stream(line.split(","))
+                    .map(StringUtils::trim)
+                    .collect(Collectors.toList());
+            classMapping.putIfAbsent(split.get(0), "Class" + classMapping.size());
+            classMapping.putIfAbsent(split.get(1), "Class" + classMapping.size());
+            edges.add(new ImmutablePair<>(classMapping.get(split.get(0)), classMapping.get(split.get(1))));
+        }
+        classes.addAll(classMapping.values());
+    }
+
+    private String getGraphString() {
+        try {
+            // Based off of https://infranodus.com/diseases/diseases
+            var stream = this.getClass().getClassLoader().getResourceAsStream("graph.csv");
+            if (stream == null) {
+                throw new IllegalStateException("Could not find graph.csv file");
+            }
+            return IOUtils.toString(stream, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not convert InputStream to String", e);
         }
     }
-    // TODO:
-    //  Implement the following in mock for now:
-    //      - Implement graph structure, is in-memory fine?
-    //      - Classes as nodes, dependencies as edges
-    //      - What about data objects?
-    //      - How do we incorporate additional information such as data size and dependency frequency?
 
     public List<String> getClasses() {
         return classes;
+    }
+
+    public List<Pair<String, String>> getEdges() {
+        return edges;
     }
 }

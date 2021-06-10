7d6bf74cc101e6325a715568d1c66cb90e8ccd1b
Pieter Dirk Soels <p.d.soels@hotmail.com>
2021-06-10 11:34:01 +0200

    Fix CBMc metric

--- src/main/java/me/soels/thesis/objectives/CouplingBetweenModuleClassesObjective.java
+++ src/main/java/me/soels/thesis/objectives/CouplingBetweenModuleClassesObjective.java
@@ -2,9 +2,10 @@ package me.soels.thesis.objectives;
 
 import me.soels.thesis.encoding.Clustering;
 import me.soels.thesis.model.EvaluationInput;
+import me.soels.thesis.tmp.daos.AbstractClass;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * Coupling between module classes (CBMC) as suggested by Lindvall et al. (2003). We map the concept of modules to
@@ -21,36 +22,38 @@ import java.util.stream.Collectors;
 public class CouplingBetweenModuleClassesObjective implements OnePurposeMetric {
     @Override
     public double calculate(Clustering clustering, EvaluationInput evaluationInput) {
-        var byClass = clustering.getByClass();
         var clusterIn = new HashMap<Integer, Set<Integer>>();
         clustering.getByCluster().keySet().forEach(clusterNumber -> clusterIn.put(clusterNumber, new HashSet<>()));
+        var clusterOut = new HashMap<Integer, Set<Integer>>();
+        clustering.getByCluster().keySet().forEach(clusterNumber -> clusterOut.put(clusterNumber, new HashSet<>()));
 
         // Create a map of incoming dependencies of other cluster for each cluster
-        clustering.getByCluster().forEach((clusterNumber, cluster) -> cluster.stream()
-                .flatMap(clazz -> clazz.getDependenceRelationships().stream())
-                .filter(relationship -> !cluster.contains(relationship.getCallee()))
-                .forEach(relationship -> clusterIn.get(byClass.get(relationship.getCallee())).add(clusterNumber)));
+        clustering.getByCluster().forEach((clusterNumber, cluster) ->
+                populateIncomingDependencies(clustering.getByClass(), clusterIn, clusterNumber, cluster));
 
-        // TODO: Now we need to also calculate the outgoing dependencies which we can do from the same map. Summing
-        //  those for each cluster and then averaging over all clusters would give the result.
-
-        // TODO: Remove old code.
-        Map<Integer, List<Integer>> interClusterDependencies = clustering.getByCluster().entrySet().stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, entry -> new ArrayList<>()));
-        var clusteringByClass = clustering.getByClass();
-        for (var edge : evaluationInput.getDependencies()) {
-            var classA = edge.getCaller();
-            var classB = edge.getCallee();
-
-            if (!clusteringByClass.get(classA).equals(clusteringByClass.get(classB))) {
-                interClusterDependencies.get(clusteringByClass.get(classB)).add(clusteringByClass.get(classA));
-                interClusterDependencies.get(clusteringByClass.get(classA)).add(clusteringByClass.get(classB));
-            }
-        }
+        populateOutgoingDependencies(clusterIn, clusterOut);
 
         return clustering.getByCluster().keySet().stream()
-                .mapToDouble(otherClasses -> interClusterDependencies.get(otherClasses).size())
+                .mapToDouble(clusterNumber -> clusterIn.get(clusterNumber).size() + clusterOut.get(clusterNumber).size())
                 .average()
                 .orElseThrow(() -> new IllegalStateException("Could not create average CBMC for this solution"));
+    }
+
+    private void populateIncomingDependencies(Map<? extends AbstractClass, Integer> byClass,
+                                              HashMap<Integer, Set<Integer>> clusterIn,
+                                              Integer clusterNumber,
+                                              List<? extends AbstractClass> cluster) {
+        cluster.stream()
+                .flatMap(clazz -> clazz.getDependenceRelationships().stream())
+                .filter(relationship -> !cluster.contains(relationship.getCallee()))
+                .forEach(relationship -> clusterIn.get(byClass.get(relationship.getCallee())).add(clusterNumber));
+    }
+
+    private void populateOutgoingDependencies(HashMap<Integer, Set<Integer>> clusterIn,
+                                              HashMap<Integer, Set<Integer>> clusterOut) {
+        clusterIn.entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream()
+                        .map(incoming -> Pair.of(incoming, entry.getKey())))
+                .forEach(pair -> clusterOut.get(pair.getKey()).add(pair.getValue()));
     }
 }
